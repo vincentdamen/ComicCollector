b323d0a2c9216e630f1e11eddd759798a538bddb
Vincent Damen <vinster1997@gmail.com>
2018-01-13 17:33:27 +0100

    Connected to API for the first time

--- app/src/main/java/com/example/vincent/comiccollector/mainActivity.java
+++ app/src/main/java/com/example/vincent/comiccollector/mainActivity.java
@@ -7,6 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class mainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
@@ -34,7 +45,32 @@ public class mainActivity extends AppCompatActivity {
             return false;
         }
     };
+    public String createLink(String query){
+        // Creates the link to access the Marvel API
+        String link = getString(R.string.apiLink)+query;
+        String timeStamp = System.currentTimeMillis()/1000 + "" ;
+        String privateKey = getString(R.string.privateKey);
+        String publicKey = getString(R.string.publicKey);
+        String hash = createHash(publicKey,privateKey,timeStamp);
 
+        return combineLink(link,publicKey,timeStamp,hash);
+    }
+    public String combineLink(String link,String publicKey, String timeStamp, String hash){
+        // Creates the full link
+        return link + "&ts="+timeStamp+"&apikey="+publicKey+"&hash="+hash;
+    }
+    public String createHash(String publicKey,String privateKey, String timeStamp){
+        // Creates the hash code to validate the keys
+        String combination = timeStamp+ privateKey+ publicKey;
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.update(combination.getBytes(),0,combination.length());
+            return new BigInteger(1, messageDigest.digest()).toString(16);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "Something went wrong";
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +79,23 @@ public class mainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        String url = createLink("");
+        RequestQueue queue = Volley.newRequestQueue(this);
 
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextMessage.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextMessage.setText(error.toString());
+            }
+        });
+        queue.add(stringRequest);
 
     }
 
